0e647762b8639d1cdd9de04c1b076cc035118066
McMATT <matux613@gmail.com>
2023-11-27 12:18:38 +0100

    27.11 mac

--- app/page.js
+++ app/page.js
@@ -1,32 +1,30 @@
 import Image from "next/image";
 import { getProducts } from "@/lib/products";
 import ProductCard from "./componets/productcard";
+import { IMAGES_MANIFEST } from "next/dist/shared/lib/constants";
+
 
 export default async function Home() {
   const products = await getProducts();
   console.log("products", products);
-  //
 
   return (
     <div className="min-h-screen flex flex-col">
       <nav className="flex justify-between pt-7 px-4 md:px-6">
-        <div className="flex items-center m-3 md:m-5 relative w-8">
+        <div className="flex items-center m-3 md:m-5">
           <a href="index.html">
-            {
-              <Image
-                className="h-12 md:h-[95px]"
-                src="/IMG/logo.png"
-                alt="patras.sk"
-                fill
-              />
-            }
+            <img
+              className="h-12 md:h-[95px]"
+              src="/IMG/logo.png"
+              alt="patras.sk"
+            />
           </a>
         </div>
         <div className="flex items-center">
           <ul className="flex ml-auto">
             <li className="m-4 md:m-5 font-plus-jakarta">
               <a
-                href=""
+                href="products.html"
                 className="no-underline text-black1 text-h7 md:text-h6"
               >
                 Produkty
@@ -43,26 +41,22 @@ export default async function Home() {
           </a>
         </div>
       </nav>
-
-      <div className="flex items-center justify-center h-8">
-        {
-          <Image
-            src="/IMG/main_frame.png"
-            alt="main"
-            className="max-w-full py-8 md:py-16"
-            fill
-          />
-        }
+      <div className="flex items-center justify-center h-full">
+        <img
+          src="/IMG/main_frame.png"
+          alt="main"
+          className="max-w-full py-8 md:py-16"
+          fill
+        />
       </div>
       <h1 className="font-plus-jakarta text-h5 self-center drop-shadow-md md:text-h1">
         Produkty
       </h1>
-      {/* <Image
+      <img
         src="/IMG/produkt-kvapky.png"
         alt=""
         className="p-0 w-28 self-center md:w-64"
-        fill
-      /> */}
+      />
       <div className="flex justify-center p-4 md:p-12 space-x-4 md:space-x-12">
         <button
           type="button"
@@ -70,30 +64,25 @@ export default async function Home() {
           className="border border-black2 rounded-3xl drop-shadow-lg p-4 w-80 hover:bg-blue2 md:p-8"
         >
           <div className="w-full justify-center">
-            <Image src="/IMG/kavomat1.png" alt="kavomat1" fill />
+            <img src="/IMG/kavomat1.png" alt="kavomat1" />
           </div>
           <h1 className="font-plus-jakarta text-center pt-8">Kavomaty</h1>
         </button>
         <button className="border border-black2 rounded-3xl drop-shadow-md w-80 hover:bg-blue2 p-4 md:p-8">
           <div className="w-full justify-center">
-            {/* <Image src="/IMG/lima1.png" alt="vodomat" fill /> */}
+            <img src="/IMG/lima1.png" alt="vodomat" />
           </div>
           <h1 className="font-plus-jakarta text-center pt-8">Davkovac vody</h1>
         </button>
         <button className="border border-black2 rounded-3xl drop-shadow-md hover:bg-blue2 p-4 md:p-8">
           <div>
-            {/* <Image
-              src="/IMG/vodovac1.png"
-              alt="vodovac"
-              className="w-full"
-              fill
-            /> */}
+            <img src="/IMG/vodovac1.png" alt="vodovac" className="w-full" />
           </div>
           <h1 className="font-plus-jakarta text-center pt-8">Vodovac</h1>
         </button>
       </div>
       <div className="flex justify-center p-4 md:p-10">
-        {/* <Image src="/IMG/frame_about.png" alt="onas" fill /> */}
+        <img src="/IMG/frame_about.png" alt="onas" />
       </div>
       <div className="flex bg-white2 w-[88%] max-w-6xl m-auto rounded-3xl justify-between md:rounded-[40px] h-24 md:h-64 max-h-64">
         <div>
@@ -113,14 +102,14 @@ export default async function Home() {
           </div>
         </div>
         <div>
-          {/* <Image
+          <img
             className="flex w-[100%] h-full"
             src="/IMG/z_vodovac.png"
             alt="img"
-            fill
-          /> */}
+          />
         </div>
       </div>
+      
     </div>
   );
 }
