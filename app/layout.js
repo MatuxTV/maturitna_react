8db969fc1a53b0cc62dcb88698820c50f18dc556
MATT <magyar.bb87@gmail.com>
2024-01-02 11:18:55 +0100

    2.1 PC

--- app/layout.js
+++ app/layout.js
@@ -5,6 +5,7 @@ import Script from "next/script";
 import Head from "next/head";
 import Link from "next/link";
 import { CartProvider } from "@/lib/cart-context";
+import { QueryClientProvider } from "react-query";
 
 const plus_jakarta = Plus_Jakarta_Sans({
   subsets: ["latin"],
@@ -29,52 +30,60 @@ export default function RootLayout({ children }) {
         <meta name="description" content={metadata.description} />
       </Head>
       <body className={plus_jakarta.className}>
-        <CartProvider>
-          <div className="hidden bg-white2 drop-shadow md:flex content-center p-2 gap-8 px-6">
-            <a className="flex items-center gap-3" href="tel:+421 905 249 998">
-              <i className="fa-solid fa-phone text-black1" />
-              <p className="font-plus-jakarta">+421 905 249 998</p>
-            </a>
-            <a
-              className="flex items-center gap-3"
-              href="mailto:patras@patras.sk"
-            >
-              <i className="fa-solid fa-envelope text-black1" />
-              <p className="font-plus-jakarta">patras@patras.sk</p>
-            </a>
-            <div className="ml-auto flex flex-row gap-4">
-              <p className="font-plus-jakarta">Kontakty</p>
-              <p className="font-plus-jakarta">O nas</p>
-            </div>
-          </div>{" "}
-          {children}
-          <footer className="flex items-center m-4 justify-center md:m-8">
-            <div className="flex items-center gap-5 md:gap-12">
-              <div className="relative hidden md:h-[95px] md:w-44 md:block">
-                <Image src="/IMG/logo.png" alt="patras.sk" fill />
+        <QueryClientProvider client={queryClient}>
+          <CartProvider>
+            <div className="hidden bg-white2 drop-shadow md:flex content-center p-2 gap-8 px-6">
+              <a
+                className="flex items-center gap-3"
+                href="tel:+421 905 249 998"
+              >
+                <i className="fa-solid fa-phone text-black1" />
+                <p className="font-plus-jakarta">+421 905 249 998</p>
+              </a>
+              <a
+                className="flex items-center gap-3"
+                href="mailto:patras@patras.sk"
+              >
+                <i className="fa-solid fa-envelope text-black1" />
+                <p className="font-plus-jakarta">patras@patras.sk</p>
+              </a>
+              <div className="ml-auto flex flex-row gap-4">
+                <p className="font-plus-jakarta">Kontakty</p>
+                <p className="font-plus-jakarta">O nas</p>
+                <Link href="/registration">
+                  <p className="font-plus-jakarta">Prihlasenie</p>
+                </Link>
               </div>
-              <p className="font-plus-jakarta text-[8px] md:text-h7">
-                patras@patras.sk
-              </p>
-              <p className="font-plus-jakarta text-[8px] md:text-h7">
-                +421 905 249 998
-              </p>
-              <p className="font-plus-jakarta flex text-[8px] md:text-h7">
-                Mlynska 34 976 Selce
-              </p>
-              <a href="">
+            </div>{" "}
+            {children}
+            <footer className="flex items-center m-4 justify-center md:m-8">
+              <div className="flex items-center gap-5 md:gap-12">
+                <div className="relative hidden md:h-[95px] md:w-44 md:block">
+                  <Image src="/IMG/logo.png" alt="patras.sk" fill />
+                </div>
                 <p className="font-plus-jakarta text-[8px] md:text-h7">
-                  Obchodne podmienky
+                  patras@patras.sk
                 </p>
-              </a>
-              <a href="">
                 <p className="font-plus-jakarta text-[8px] md:text-h7">
-                  Podmienky reklamacie
+                  +421 905 249 998
                 </p>
-              </a>
-            </div>
-          </footer>
-        </CartProvider>
+                <p className="font-plus-jakarta flex text-[8px] md:text-h7">
+                  Mlynska 34 976 Selce
+                </p>
+                <a href="">
+                  <p className="font-plus-jakarta text-[8px] md:text-h7">
+                    Obchodne podmienky
+                  </p>
+                </a>
+                <a href="">
+                  <p className="font-plus-jakarta text-[8px] md:text-h7">
+                    Podmienky reklamacie
+                  </p>
+                </a>
+              </div>
+            </footer>
+          </CartProvider>
+        </QueryClientProvider>
       </body>
     </html>
   );
---
+++ app/registration/page.js
@@ -0,0 +1,96 @@
+"use client";
+import React, { useState } from "react";
+import { users } from "../../lib/users.js";
+import {useMutation} from "react-query";
+
+const RegisterPage = () => {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [error, setError] = useState("");
+
+  const singUpMutation = useMutation((newUser) => {
+    setData(users, { data: newUser }, "/system").then((response) => {
+      console.log(response);
+    });
+  });
+
+  const handleRegister = async (e) => {
+    e.preventDefault();
+
+    singUpMutation.mutate({
+      email: e.target.email.value,
+      password: e.target.password.value,
+      role: "bb4da356-49ec-44b3-88af-5c8612676ae4",
+      status: "active",
+      provider: "",
+    });
+  };
+
+  // try {
+  //   // Vložte logiku pre registráciu používateľa do databázy
+  //   const response = await directus.items('users').create({
+  //     email,
+  //     password,
+  //     // pridať ďalšie polia podľa potreby
+  //   });
+  //   console.log(response);
+  //   // Presmerovať používateľa alebo zobraziť správu o úspechu
+  // } catch (err) {
+  //   setError(err.message);
+  // }
+
+  return (
+    <div className="min-h-screen flex items-center justify-center bg-gray-100">
+      <form
+        className="bg-white shadow-md rounded px-8 pt-6 pb-8 mb-4"
+        onSubmit={handleRegister}
+      >
+        <div className="mb-4">
+          <label
+            className="block text-gray-700 text-sm font-bold mb-2"
+            htmlFor="username"
+          >
+            Meno
+          </label>
+          <input
+            className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+            id="username"
+            type="text"
+            placeholder="Meno"
+            value={username}
+            onChange={(e) => setUsername(e.target.email.value)}
+            required
+          />
+        </div>
+        <div className="mb-6">
+          <label
+            className="block text-gray-700 text-sm font-bold mb-2"
+            htmlFor="password"
+          >
+            Heslo
+          </label>
+          <input
+            className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 mb-3 leading-tight focus:outline-none focus:shadow-outline"
+            id="password"
+            type="password"
+            placeholder="Heslo"
+            value={password}
+            onChange={(e) => setPassword(e.target.password.value)}
+            required
+          />
+        </div>
+        <div className="flex items-center justify-between">
+          <button
+            className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline"
+            type="submit"
+          >
+            Registrovať
+          </button>
+        </div>
+        {error && <p className="text-red-500 text-xs italic">{error}</p>}
+      </form>
+    </div>
+  );
+};
+
+export default RegisterPage;
