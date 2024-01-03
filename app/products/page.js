a9d63e4b0a72a1b56cb7aaec4bb5c38814edc049
MATT <magyar.bb87@gmail.com>
2024-01-03 11:33:11 +0100

    2.1FINAL

--- app/products/page.js
+++ app/products/page.js
@@ -1,80 +1,58 @@
-"use client";
-import React from "react";
-import { useQuery } from "react-query";
 import Nav from "../componets/nav";
 import Product from "../componets/product";
 import Link from "next/link";
 import { toast, ToastContainer } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
-import queryClient from "@/lib/queryClient";
-import { QueryClientProvider } from "react-query";
 
-const fetchProducts = async (category) => {
-  const response = await fetch(
-    `${process.env.NEXT_PUBLIC_DIRECTUS}items/produkty?filter[kategoria][id][_eq]=${category}`,
-    { cache: "no-store" }
-  );
-  return response.json();
-};
-
-const fetchCategory = async (category) => {
-  const response = await fetch(
-    `${process.env.NEXT_PUBLIC_DIRECTUS}items/kategoria/${category}`,
-    { cache: "no-store" }
-  );
-  return response.json();
-};
-
-const Produkty = ({ searchParams }) => {
+export const Produkty = async ({ searchParams }) => {
   const category = searchParams.kategoria;
 
-  // Použitie React Query na načítanie produktov
-  const { data: products, isLoading: isLoadingProducts } = useQuery(
-    ["products", category],
-    () => fetchProducts(category)
-  );
-
-  // Použitie React Query na načítanie kategórie
-  const { data: kategoria, isLoading: isLoadingCategory } = useQuery(
-    ["category", category],
-    () => fetchCategory(category)
-  );
-
-  if (isLoadingProducts || isLoadingCategory) return "Načítavam...";
-
-  // Zobrazovanie toastov, ak je to potrebné
-  if (kategoria.error) toast.error("Chyba pri načítaní kategórie.");
+  function getProducts() {
+    return fetch(
+      process.env.NEXT_PUBLIC_DIRECTUS + `items/produkty?filter[kategoria][id][_eq]=${category}`,
+      {
+        cache: "no-store",
+      }
+    ).then((res) => res.json());
+  }
+  function getCategory() {
+    return fetch(
+      process.env.NEXT_PUBLIC_DIRECTUS + `items/kategoria/${category}`,
+      {
+        cache: "no-store",
+      }
+    ).then((res) => res.json());
+  }
+  const products = await getProducts();
+  const kategoria = await getCategory();
 
   console.log(kategoria);
 
   return (
-    <QueryClientProvider client={queryClient}>
+    <div>
+      <Nav product={"Produkty"} />
+      <ToastContainer
+        position="top-right"
+        autoClose={3000}
+        hideProgressBar={false}
+        newestOnTop={false}
+        closeOnClick
+        rtl={false}
+        zIndex={1}
+      />
       <div>
-        <Nav product={"Produkty"} />
-        <ToastContainer
-          position="top-right"
-          autoClose={3000}
-          hideProgressBar={false}
-          newestOnTop={false}
-          closeOnClick
-          rtl={false}
-          zIndex={1}
-        />
-        <div>
-          <Link href="/products-cat">
-            <p className="flex text-h4 m-10 font-plus-jakarta">
-              Produkty/{kategoria.data?.nazov}
-            </p>
-          </Link>
-          <div className="flex flex-wrap justify-start m-8 space-x-5">
-            {products.data?.map((item) => (
-              <Product {...item} key={item.id} />
-            ))}
-          </div>
+        <Link href="/products-cat">
+          <p className="flex text-h4 m-10 font-plus-jakarta">
+            Produkty/{kategoria.data.nazov}
+          </p>
+        </Link>
+        <div className="flex flex-wrap justify-start m-8 space-x-5">
+          {products.data?.map((item) => {
+            return <Product {...item} key={item.id} />;
+          })}
         </div>
       </div>
-      </QueryClientProvider>
+    </div>
   );
 };
-
 export default Produkty;
--- app/registration/page.js
+++ app/registration/page.js
@@ -1,100 +1,167 @@
-'use client'
+"use client";
 import React, { useState } from "react";
-import { users } from "../../lib/users.js";
-import { useMutation } from "react-query";
-import queryClient from "@/lib/queryClient.js";
-import { QueryClientProvider } from "react-query";
+import directus from "/lib/directus"; // cesta k vašej Directus inštancii
+import Image from "next/image";
+import Nav from "../componets/nav";
+import Link from "next/link";
+// import { createUser } from "@directus/sdk";
+// import { createDirectus, rest, login, refresh, logout} from '@directus/sdk';
 
 const RegisterPage = () => {
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
   const [error, setError] = useState("");
 
-  // const singUpMutation = useMutation((newUser) => {
-  //   setData(users, { data: newUser }, "/system").then((response) => {
-  //     console.log(response);
-  //   });
-  // });
+  const handleRegister = async (e) => {
+    try {
+      const user_object = {
+        email: email,
+        password: password,
+        role: "bb4da356-49ec-44b3-88af-5c8612676ae4", // ID vašej špecifické role
+        // Pridajte ďalšie požadované polia podľa potreby
+      };
 
-  // const handleRegister = async (e) => {
-  //   e.preventDefault();
+      const result = await directus.request(createUser(user_object));
+      console.log("Registrácia úspešná, odpoveď:", result);
+      // Tu by ste mohli pridať presmerovanie alebo správu o úspechu
+    } catch (err) {
+      console.error("Chyba pri registrácii:", err);
+      // Spracujte a zobrazte chybu
+    }
+  };
+  // const RegisterPage = () => {
+  //   const [email, setEmail] = useState("");
+  //   const [password, setPassword] = useState("");
+  //   const [error, setError] = useState("");
 
-  //   singUpMutation.mutate({
-  //     email: e.target.email.value,
-  //     password: e.target.password.value,
-  //     role: "bb4da356-49ec-44b3-88af-5c8612676ae4",
-  //     status: "active",
-  //     provider: "",
-  //   });
-  // };
+  //   const handleRegister = async (e) => {
+  //     e.preventDefault();
+  //     try {
 
-  // try {
-  //   // Vložte logiku pre registráciu používateľa do databázy
-  //   const response = await directus.items('users').create({
-  //     email,
-  //     password,
-  //     // pridať ďalšie polia podľa potreby
-  //   });
-  //   console.log(response);
-  //   // Presmerovať používateľa alebo zobraziť správu o úspechu
-  // } catch (err) {
-  //   setError(err.message);
-  // }
+  //       // const user = await fetch()
+
+  //       // const user = await directus.request(login(email, password));
+  //       // const authData = await directus.request(refresh(mode, refresh_token));
+  //       // await directus.request(logout(refresh_token));
+
+  //       // fetch('http://localhost:8055/auth/login', {
+  //       //   method: 'POST',
+  //       //   headers: {
+  //       //     'Content-Type': 'application/json',
+  //       //   },
+  //       //   body: JSON.stringify({
+  //       //     email: 'your-email@example.com',
+  //       //     password: 'yourpassword',
+  //       //   }),
+  //       // })
+  //       // .then(response => response.json())
+  //       // .then(data => console.log(data))
+  //       // .catch((error) => console.error('Error:', error));
+  //       // Vytvorenie používateľa pomocou funkcie createUser poskytnutej Directus SDK
+  //       // const response = await directus.request(
+  //       //   createUser({
+  //       //     email,
+  //       //     password,
+  //       //     role: "bb4da356-49ec-44b3-88af-5c8612676ae4",
+  //       //   },)
+  //       // );
+  //       console.log(response);
+  //       // Presmerovať používateľa alebo zobraziť správu o úspechu
+  //     } catch (err) {
+  //       setError(err.message);
+  //     }
+  //   };
 
   return (
-    <QueryClientProvider client={queryClient}>
-      <div className="min-h-screen flex items-center justify-center bg-gray-100">
-        <form
-          className="bg-white shadow-md rounded px-8 pt-6 pb-8 mb-4"
-          // onSubmit={handleRegister}
-        >
-          <div className="mb-4">
-            <label
-              className="block text-gray-700 text-sm font-bold mb-2"
-              htmlFor="username"
-            >
-              Email
-            </label>
-            <input
-              className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
-              id="email"
-              type="email"
-              placeholder="Email"
-              value={username}
-              onChange={(e) => setEmail(e.target.email.value)}
-              required
-            />
-          </div>
-          <div className="mb-6">
-            <label
-              className="block text-gray-700 text-sm font-bold mb-2"
-              htmlFor="password"
-            >
-              Heslo
-            </label>
-            <input
-              className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 mb-3 leading-tight focus:outline-none focus:shadow-outline"
-              id="password"
-              type="password"
-              placeholder="Heslo"
-              value={password}
-              onChange={(e) => setPassword(e.target.password.value)}
-              required
-            />
-          </div>
-          <div className="flex items-center justify-between">
-            <button
-              className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline"
-              type="submit"
-            >
-              Registrovať
-            </button>
-          </div>
-          {error && <p className="text-red-500 text-xs italic">{error}</p>}
-        </form>
+    <>
+      <Nav />
+      <div className=" min-h-fit flex items-center justify-center bg-gray-100">
+        <div className="flex flex-col">
+          <Image
+            className="m-8"
+            src="/IMG/logo.png"
+            alt="logo"
+            width={250}
+            height={250}
+          />
+
+          <form
+            className="bg-white2 shadow-md rounded-lg px-8 pt-6 pb-8 mb-4"
+            onSubmit={handleRegister}
+          >
+            <div className=" text-center">
+              <p className="  text-h4 text-black1 font-plus-jakarta m-4 ">
+                Registrácia
+              </p>
+            </div>
+
+            <div className="mb-4">
+              <label
+                className="block text-gray-700 text-sm font-plus-jakarta mb-2"
+                htmlFor="email"
+              >
+                Meno
+              </label>
+              <input
+                className="shadow appearance-none  rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+                id="email"
+                type="email"
+                placeholder="E-mail"
+                value={email}
+                onChange={(e) => setEmail(e.target.value)}
+                required
+              />
+            </div>
+            <div className="mb-6">
+              <label
+                className=" font-plus-jakarta block text-black1 text-sm mb-2"
+                htmlFor="password"
+              >
+                Heslo
+              </label>
+              <input
+                className="shadow appearance-none  rounded w-full py-2 px-3 text-black1 mb-3 leading-tight focus:outline-none focus:shadow-outline"
+                id="password"
+                type="password"
+                placeholder="Heslo"
+                value={password}
+                onChange={(e) => setPassword(e.target.value)}
+                minLength={8}
+                pattern="(?=.*\d)(?=.*[a-z])(?=.*[A-Z]).{8,}"
+                required
+              />
+            </div>
+
+            <div className=" align-middle ">
+              <input type="checkbox" id="terms" required />
+              <label
+                className=" font-plus-jakarta text-black1 mx-2 "
+                htmlFor="terms"
+              >
+                Súhlasím s podmienkami PATRAS.SK
+              </label>
+            </div>
+            <div className="flex items-center justify-center">
+              <button
+                className=" font-plus-jakarta m-5 bg-blue1 p-2 rounded-lg hover:bg-blue2"
+                type="submit"
+              >
+                Registrovať
+              </button>
+            </div>
+            {error && <p className=" text-red text-xs italic">{error}</p>}
+            <div className=" justify-start mt-3">
+              <p>
+                Už máš účet?{" "}
+                <Link href="/login" className="text-blue1">
+                  <p>Prihlás sa</p>
+                </Link>
+              </p>
+            </div>
+          </form>
+        </div>
       </div>
-     </QueryClientProvider>
+    </>
   );
 };
-
 export default RegisterPage;
